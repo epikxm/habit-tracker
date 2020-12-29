ff3c45b5fe4145b47136de19e8a406a6095003ad
Ki-nam Bang <epikxm@gmail.com>
2020-12-29 13:59:47 +0900

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,68 @@
+import './app.css';
+import Habits from './components/habits';
+
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+
+class App extends Component {
+  state = {
+    habits: [{
+        id: 0, name: 'Reading', count: 0
+    }, {
+        id: 1, name: 'Running', count: 0
+    }, {
+        id: 2, name: 'Coding', count: 0
+    }]
+  };
+
+  handleIncreament = habit => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({habits});
+  };
+
+  handleDecreament = habit => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count -1 ;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({habits});
+  };
+
+  handleDelete = habit => {
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
+    this.setState({habits});
+  };
+
+  handleAdd = name => {
+    const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];
+    this.setState({ habits });
+  };
+
+  handleReset = () => {
+    const habits = this.state.habits.map(habit => {
+      habit.count = 0;
+      return habit;
+    })
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar totalCount={ this.state.habits.filter(item => item.count > 0 ).length }/>
+        <Habits
+          habits={this.state.habits}
+          onIncreament={this.handleIncreament}
+          onDecreament={this.handleDecreament}
+          onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
+          onReset={this.handleReset}
+        />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/habits.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Habit from './habit';
+import HabitAddForm from './habitAddForm';
+
+class Habits extends Component {
+    handleIncreament = habit => {
+        this.props.onIncreament(habit);
+    };
+
+    handleDecreament = habit => {
+        this.props.onDecreament(habit);
+    };
+
+    handleDelete = habit => {
+        this.props.onDelete(habit);
+    };
+
+    handleAdd = name => {
+        this.props.onAdd(name);
+    };
+
+    render() {
+        return (
+            <>
+                <HabitAddForm onAdd={this.handleAdd}/>
+                <ul>
+                    {this.props.habits.map(habit => (
+                        <Habit
+                            key={habit.id}
+                            habit = {habit}
+                            onIncreament={this.handleIncreament}
+                            onDecreament={this.handleDecreament}
+                            onDelete={this.handleDelete}
+                        />
+                    ))}
+                </ul>
+                <button className="habits-reset" onClick={this.props.onReset}>RESET</button>
+            </>
+        );
+    };
+}
+
+export default Habits;
